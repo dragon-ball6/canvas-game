5393d0a255b0a21ef05467c97ffadaae629afb56
dragon-ball6 <dominicchanzy@gmail.com>
2022-04-04 08:24:26 +0800

    Player movement

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ let setup = () => {
   createCanvas(500, 500);
 };
 
+// key pressed
+let keyP = {};
+
 let world = { x: 1000, y: 1000 };
 
 class Player {
@@ -9,16 +12,34 @@ class Player {
     this.x = world.x/2;
     this.y = world.y/2;
     this.size = 30;
+    this.spd = 5;
   }
   display() {
     ellipse(this.x, this.y, this.size, this.size);
   }
   update() {
-    
+    let spd = constrain(this.spd, this.size/2, this.world-this.size/2);
+
+    if (keyP.a) this.x -= spd;
+    if (keyP.d) this.x += spd;
+    if (keyP.w) this.y -= spd;
+    if (keyP.s) this.y += spd;
   }
 }
 
+let you = new Player();
+
 let draw = () => {
   background(255);
+  you.display();
+  you.update();
+};
 
+let keyPressed = () => {
+  keyP[String(key).toLowerCase()] = true;
+  return false;
+};
+let keyReleased = () => {
+  delete keyP[String(key).toLowerCase()];
+  return false;
 };
