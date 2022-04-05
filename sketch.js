511c583d6135e64d23a9146af488c479563e44e9
dragon-ball6 <dominicchanzy@gmail.com>
2022-04-05 09:13:43 +0800

    More stuff

--- sketch.js
+++ sketch.js
@@ -7,6 +7,16 @@ let keyP = {};
 
 let world = { x: 1000, y: 1000 };
 
+let resources = {
+  wood: 0,
+  stone: 0,
+  gold: 0,
+  gems: 0,
+  fur: 0,
+  meat: 0
+};
+
+// some sugar to remove our attention from the prevailing truth =]
 class Player {
   constructor() {
     this.x = world.x/2;
@@ -15,25 +25,42 @@ class Player {
     this.spd = 5;
   }
   display() {
-    fill(0xFFDBAC);
-    ellipse(this.x, this.y, this.size, this.size);
+    fill(unhex("FFDBAC"));
+    ellipse(width/2, height/2, this.size, this.size);
   }
   update() {
-    let spd = constrain(this.spd, this.size/2, this.world-this.size/2);
+    if (keyP.a) this.x = constrain(this.x - this.spd, this.size/2, this.world-this.size/2);
+    if (keyP.d) this.x = constrain(this.x + this.spd, this.size/2, this.world-this.size/2);
+    if (keyP.w) this.y = constrain(this.y - this.spd, this.size/2, this.world-this.size/2);
+    if (keyP.s) this.y = constrain(this.y + this.spd, this.size/2, this.world-this.size/2);
+  }
+}
 
-    if (keyP.a) this.x -= spd;
-    if (keyP.d) this.x += spd;
-    if (keyP.w) this.y -= spd;
-    if (keyP.s) this.y += spd;
+class Generator {
+  constructor(x, y, type, health) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    this.health = health;
+  }
+  mine() {
+    this.health -= 10;
+    resources[this.type] += 1;
   }
 }
 
 let you = new Player();
 
 function draw() {
-  background(255);
-  you.display();
-  you.update();
+  // try-catch: MY PRECIOUS
+  try {
+    background(255);
+    you.display();
+    you.update();
+  } catch (e) {
+    console.error(`The DEVELOPER made a mistake!!! ${e.message} at ${e.line}`);
+    console.info("If you are not the DEVELOPER, create an issue here: https://github.com/dragon-ball6/canvas-game/issues")
+  }
 }
 
 function keyPressed() {
